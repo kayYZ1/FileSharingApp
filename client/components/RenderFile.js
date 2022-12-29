714292fb92f01782a9cbf64106b3768f443acf73
Kacper <106306274+kayYZ1@users.noreply.github.com>
2022-12-29 13:52:30 +0100

    feat: File upload
    To fix! xhr 500 internal error

---
+++ client/components/RenderFile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RenderFile = ({file:{format, sizeInBytes, name}}) => {
+  return (
+    <div className="flex items-center w-full p-4 my-2">
+        <img src={`/assets/images/${format}.png`} className="w-14 h-14" alt="" />
+        <span className="mx-2">{name}</span>
+        <span className="ml-auto">{sizeInMb(sizeInBytes)}</span>
+    </div>
+  )
+}
+
+const sizeInMb = (bytes) => `${(bytes/(1024*1024)).toFixed(2)} Mb`
+
+
+export default RenderFile
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,50 @@
+import { useState } from "react"
+import axios from "axios"
+
 import DropZoneComponent from "../components/DropZoneComponent.js"
+import RenderFile from "../components/RenderFile.js"
 
 export default function Home() {
+  const [file, setFile] = useState(null)
+  const [id, setId] = useState(null)
+  const [downloadPageLink, setDownloadPageLink] = useState(null)
+  const [uploadState, setUploadState] = useState(null)
+
+  const handleUpload = async () => {
+    if (uploadState === "Uploading") return
+    const formData = new FormData()
+    formData.append("myFile", file)
+    try {
+      const { data } = await axios({
+        method: "post",
+        data: formData,
+        url: "api/files/upload",
+        headers: {
+          "Content-Type": "multipart/form-data; boundary=MyBoundary"
+        }
+      })
+      setDownloadPageLink(data.downloadPageLink)
+      setId(data.id)
+    } catch (error) {
+      console.log(error.response.data)
+      setUploadState("Upload failed")
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="my-4 text-3xl font-medium">Share a pdf</h1>
       <div className="w-96 flex flex-col items-center bg-slate-100 shadow-xl rounded-xl justify-center">
-        <DropZoneComponent/>
+        <DropZoneComponent setFile={setFile}/>
+        
+        {file &&
+        <RenderFile file={{
+          format: file.type.split("/")[1],
+          name: file.name,
+          sizeInBytes: file.size
+        }} />}
+
+        <button className="w-44 my-5 bg-slate-300 p-2 rounded-md focus:outline-none" onClick={handleUpload}>Upload</button>
       </div>
     </div>
   )
