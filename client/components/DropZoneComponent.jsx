c5c4bb2cee153adae277d3922686d8f45b841ea8
kayYZ1 <overek7133@gmail.com>
2023-03-30 19:24:43 +0200

    feat: Changing the route in server.js

--- client/components/DropZoneComponent.jsx
+++ client/components/DropZoneComponent.jsx
@@ -1,39 +1,52 @@
-import React, { useCallback } from 'react'
-import { useDropzone } from 'react-dropzone'
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
-const DropZoneComponent = ({setFile}) => {
-    const onDrop = useCallback((acceptedFiles) => {
-        console.log(acceptedFiles)
-        setFile(acceptedFiles[0])
-    }, []);
+const DropZoneComponent = ({ setFile }) => {
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+    setFile(acceptedFiles[0]);
+  }, []);
 
-    const { getRootProps, getInputProps, isDragAccept, isDragReject } = useDropzone({
-        onDrop, 
-        multiple: false, 
-        accept: {'application/pdf': ['.pdf']}
-    })
+  const { getRootProps, getInputProps, isDragAccept, isDragReject } =
+    useDropzone({
+      onDrop,
+      multiple: false,
+      accept: { "application/pdf": [".pdf"] },
+    });
 
-    return (
-        <div className="p-4 w-full">
-            <div {...getRootProps()} className="w-full h-80 rounded-md cursor-pointer focus:outline-none">
-                <input {...getInputProps()}/>
-                <div className={"flex flex-col h-full space-y-3 border-2 border-slate-300 rounded-xl items-center justify-center"
-                 + (isDragReject === true ? "border-red-500":"") 
-                 + (isDragAccept === true ? "border-green-500":"")
-                }>
-                    <img src="/assets/images/folder.png" alt="folder" className="w-16 h-16"/>
-                    {isDragReject ? (
-                        <p>Only pdfs are allowed</p> ) : (
-                            <div>
-                                <p>Drag and drop your file</p>
-                                <p className="mt-2 text-base text-slate-500">Only .pdf extension will be accepted.</p>
-                            </div>
-                        )
-                    }
-                </div>
+  return (
+    <div className="p-4 w-full">
+      <div
+        {...getRootProps()}
+        className="w-full h-80 rounded-md cursor-pointer focus:outline-none"
+      >
+        <input {...getInputProps()} />
+        <div
+          className={
+            "flex flex-col h-full space-y-3 border-2 border-slate-300 rounded-xl items-center justify-center" +
+            (isDragReject === true ? "border-red-500" : "") +
+            (isDragAccept === true ? "border-green-500" : "")
+          }
+        >
+          <img
+            src="/assets/images/folder.png"
+            alt="folder"
+            className="w-16 h-16"
+          />
+          {isDragReject ? (
+            <p>Only pdfs are allowed</p>
+          ) : (
+            <div>
+              <p>Drag and drop your file</p>
+              <p className="mt-2 text-base text-slate-500">
+                Only .pdf extension will be accepted.
+              </p>
             </div>
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default DropZoneComponent
+export default DropZoneComponent;
--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(
     extended: true,
   })
 );
-app.use("/api/files", fileRoute);
+app.use("/file", fileRoute);
 
 const PORT = process.env.PORT;
 
